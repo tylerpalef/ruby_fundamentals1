a1394f6d79f92625c907d60343327bfe34757bd9
“Tyler <tylerpalef@gmail.com>
2018-07-16 16:10:05 -0400

    added challenge 1

---
+++ challenge1.rb
@@ -0,0 +1,10 @@
+puts "hey girl, what's your favourite number?"
+
+girl_number = gets.chomp.to_i
+
+if girl_number <= 100
+  print "dream bigger"
+
+elsif girl_number > 100
+  print "that's a big number girl"
+end
--- exercise3.rb
+++ exercise3.rb
@@ -1,12 +1,36 @@
-puts "What is your name?"
-user_name = gets.chomp.upcase
+# puts "What is your name?"
+# user_name = gets.chomp.upcase
+#
+# puts "Hello, #{user_name}"
+#
+#
+# puts "Give me a number"
+# my_number = gets.chomp.to_f
+#
+# if my_number > 1
+#   puts "The number is greater than 1"
+# end
+#
+# number = gets.to_i # the user types in a number
+#
+# if number > 0
+#   puts "#{number} is positive" # this line executes if the user enters a positive number
+# else
+#   puts "#{number} is negative" # this line executes if the user enters a negative number
+# end
 
-puts "Hello, #{user_name}"
-
-
-puts "Give me a number"
-my_number = gets.chomp.to_f
-
-if my_number > 1
-  puts "The number is greater than 1"
-end
+# puts "what is x value"
+#
+# x = gets.to_i
+#
+# puts "what is y value"
+#
+# y = gets.to_i
+#
+# if x > y
+#   puts "x is greater than y!"
+# elsif x < y
+#   puts "x is less than y!"
+# else
+#   puts "x equals y!"
+# end
