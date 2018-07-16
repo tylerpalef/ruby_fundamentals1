6970d28486ca16c2e3fedc6c2ae787c98910de64
“Tyler <tylerpalef@gmail.com>
2018-07-16 14:29:17 -0400

    string interpolation

--- exercise2.rb
+++ exercise2.rb
@@ -1,7 +1,10 @@
-# Leaving a tip
+# Leaving a tip, need to declare what the tip was
 tip = 0.15 * 55.00
 puts tip
 
 #String + intiger
 
 puts "hello" + 10.to_s
+
+#string interpolation
+puts "look at this number #{45628*7839}"
