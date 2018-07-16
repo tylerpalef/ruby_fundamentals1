3cb0d9c2fcfc47dd88da481d6819a39ccb2fd16a
“Tyler <tylerpalef@gmail.com>
2018-07-16 15:54:27 -0400

    finishing exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -2,3 +2,11 @@ puts "What is your name?"
 user_name = gets.chomp.upcase
 
 puts "Hello, #{user_name}"
+
+
+puts "Give me a number"
+my_number = gets.chomp.to_f
+
+if my_number > 1
+  puts "The number is greater than 1"
+end
